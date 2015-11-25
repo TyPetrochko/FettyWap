7389b6e458723fcf46c46840ee847f5839fc1e5c
Tyler Petrochko <ty.petrochko@gmail.com>
2015-11-25 12:15:08 -0500

    Cleaned up joined texts with some regexes, going to init rails app.

--- datafyer.py
+++ datafyer.py
@@ -4,6 +4,8 @@
 
 import csv
 import sys
+import re
+import HTMLParser
 from itertools import izip
 
 def get_tweets_from_csv(csv_name):
@@ -17,10 +19,27 @@ def get_tweets_from_csv(csv_name):
 				pass
 
 if __name__ == '__main__':
+	text = ""
 	g1 = get_tweets_from_csv(sys.argv[1])
 	g2 = get_tweets_from_csv(sys.argv[2])
 
 	for a, b in izip(g1, g2):
-		print a
-		print b
+		text +=  a + "\n"
+		text +=  b + "\n"
 
+	# Remove non-ascii data
+	text =  ''.join([i if ord(i) < 128 else '' for i in text])
+
+	# Remove URLs
+	text = re.sub("((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)", ""	, text)
+
+	# Remove URLs my way
+	text = re.sub("(^)?http[a-zA-Z0-9\.\/:]*($)?", "" , text, re.M)
+
+	# Remove any http/https stubs
+	text = re.sub("http|https", "", text)
+
+	# Expand HTML entities
+	text = HTMLParser.HTMLParser().unescape(text)
+
+	print text
