5b74ec6ace59ac4ee99dc827285345ca70b05f55
Tyler Petrochko <ty.petrochko@gmail.com>
2015-11-28 14:40:55 -0500

    Changed root page of project to tweet_sources#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :tweets, only: [:index, :create, :show]
 
+  root 'tweet_sources#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
